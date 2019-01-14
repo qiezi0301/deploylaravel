5a34a242e2cca78fab5904af1bf255c850cc46b4
qiezi0301 <qiezi0301@163.com>
2019-01-14 15:46:52 +0800

    new www

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,13 +1,12 @@
-@servers(['web-1' => 'root@192.168.31.251', 'web-2' => 'root@192.168.31.252'])
+@servers(['web-1' => 'root@192.168.31.251', 'web-2' => 'root@192.168.31.252', 'web-3' => 'root@192.168.31.202'])
 
-@task('deploy', ['on' => ['web-1', 'web-2'], 'parallel' => true])
-    cd /home/www/deploylaravel
+@task('deploy', ['on' => ['web-1', 'web-2', 'web-3'], 'parallel' => true])
+    cd /var/www/deploylaravel
     git reset --hard
     git pull origin {{ $branch }}
-    chown -R www:www /home/www
-    chmod -R 755 /home/www
+    chown -R www /var/www
     su - www
-    cd /home/www/deploylaravel
+    cd /var/www/deploylaravel
     composer install --no-dev
     php artisan migrate --force
 @endtask
